e107934ddaa8bb5a4bc93824412df37f341a43cc
Juwel-771 <juwelah99@gmail.com>
2025-11-21 03:08:00 +0600

    habitable planet function created

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const {parse} = require('csv-parse');
 const fs = require('fs');
 
-const result = [];
+const habitablePlanet = [];
+
+function isHabitablePlanet(planet){
+    return planet["koi_disposition"] === "CONFIRMED"
+           && planet["koi_insol"] > 0.36 
+           && planet["koi_insol"] < 1.11 
+           && planet["koi_prad"] < 1.6;
+}
 
 fs.createReadStream('kepler_data.csv')
     .pipe(parse({
@@ -9,14 +16,15 @@ fs.createReadStream('kepler_data.csv')
         columns: true,
     }))
     .on('data', (data)=> {
-        result.push(data);
+        if(isHabitablePlanet(data)){
+            habitablePlanet.push(data);
+        }
     })
     .on('error', (err)=> {
         console.log(err);
     })
     .on('end', ()=> {
-        console.log(result);
-        console.log("All Done");
+        console.log(`${habitablePlanet.length} habitable planet found`);
     })
 
 // parse();
